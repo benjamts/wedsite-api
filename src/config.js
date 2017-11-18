12cc13104b31a0142cfcaf8ab008742aa5a65462
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-11-17 19:12:41 -0800

    Configure Node-Postgres

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
+exports.DATABASE_URL = process.env.DATABASE_URL
 exports.PORT = process.env.PORT
---
+++ src/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg')
+const config = require('./config');
+
+
+const pool = new Pool({
+  connectionString: config.DATABASE_URL,
+})
+
+
+module.exports = pool;
