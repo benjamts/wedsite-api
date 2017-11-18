b74d62dc7199f6ed552d61189964656329ee1d9c
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-11-18 12:19:18 -0800

    Make SQL Template Tag Support Interpolation
    It's not for unescaped parameters, don't worry!

--- src/sql-template-tag.js
+++ src/sql-template-tag.js
@@ -1,3 +1,9 @@
-module.exports = function sql (strings) {
-  return strings[0].replace(/\s+/g, ' ').trim()
+module.exports = function sql (strings, ...values) {
+  let sql = ''
+  for (let i = 0; i < values.length; i++) {
+    sql += strings[i]
+    sql += values[i]
+  }
+  sql += strings[strings.length - 1]
+  return sql.replace(/\s+/g, ' ').trim()
 }
---
+++ test/sql-template-tag.js
@@ -0,0 +1,44 @@
+/* eslint-env mocha */
+const assert = require('assert')
+const sql = require('../src/sql-template-tag')
+
+describe('sql template tag', function () {
+  it('strips superfluous whitespace', function () {
+    assert.equal(sql`
+      SELECT 1
+      FROM foo_table
+      WHERE stuff IS TRUE;
+    `, 'SELECT 1 FROM foo_table WHERE stuff IS TRUE;')
+  })
+
+  it('handles interpolation', function () {
+    assert.equal(sql`
+      SELECT 1
+      FROM foo_table
+      WHERE stuff IS ${'TRUE'};
+    `, 'SELECT 1 FROM foo_table WHERE stuff IS TRUE;')
+  })
+
+  it('handles lots of interpolation', function () {
+    assert.equal(sql`
+      ${'SELECT'} ${1}
+      FROM foo_table
+      WHERE stuff IS ${'TRUE;'}
+    `, 'SELECT 1 FROM foo_table WHERE stuff IS TRUE;')
+  })
+
+  it('handles leading interpolation', function () {
+    assert.equal(sql`${'SELECT'} ${1}
+      FROM foo_table
+      WHERE stuff IS ${'TRUE;'}
+    `, 'SELECT 1 FROM foo_table WHERE stuff IS TRUE;')
+  })
+
+  it('handles trailing interpolation', function () {
+    assert.equal(sql`${'SELECT'} ${1}
+      FROM foo_table
+      WHERE stuff IS ${'TRUE;'}`,
+      'SELECT 1 FROM foo_table WHERE stuff IS TRUE;'
+    )
+  })
+})
