a66e6acb96fca7a0c8635be0bac354a8fb82a8eb
Tyler Benjamin <tyler.scott.benjamin@gmail.com>
2017-12-06 20:17:03 -0800

    Configure CORS

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const config = require('./src/config')
+const cors = require('cors');
 const express = require('express')
 
 const app = express()
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' ? 'https://tylerandsarah.com' : 'http://localhost:8080',
+  optionsSuccessStatus: 200,
+}))
 
+app.options('*', cors())
 app.use(require('./src/routes'))
 
 app.listen(config.PORT)
